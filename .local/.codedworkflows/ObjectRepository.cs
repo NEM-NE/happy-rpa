a50edd168f7067f95f554f338099beafb991a01a
wonholim <kds0034@gmail.com>
2024-06-13 15:51:14 +0900

    종강: 경축

---
+++ .local/.codedworkflows/ObjectRepository.cs
@@ -0,0 +1,33 @@
+using UiPath.CodedWorkflows.DescriptorIntegration;
+
+namespace demo.ObjectRepository
+{
+    public static class Descriptors
+    {
+    }
+}
+
+namespace demo._Implementation
+{
+    internal class ScreenDescriptorDefinition : IScreenDescriptorDefinition
+    {
+        public IScreenDescriptor Screen { get; set; }
+
+        public string Reference { get; set; }
+
+        public string DisplayName { get; set; }
+    }
+
+    internal class ElementDescriptorDefinition : IElementDescriptorDefinition
+    {
+        public IScreenDescriptor Screen { get; set; }
+
+        public string Reference { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public IElementDescriptor ParentElement { get; set; }
+
+        public IElementDescriptor Element { get; set; }
+    }
+}
---
+++ .local/.codedworkflows/WorkflowRunnerService.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UiPath.CodedWorkflows;
+using UiPath.Activities.Contracts;
+
+namespace demo
+{
+    public class WorkflowRunnerService
+    {
+        private readonly Func<string, IDictionary<string, object>, TimeSpan?, bool, InvokeTargetSession, IDictionary<string, object>> _runWorkflowHandler;
+        public WorkflowRunnerService(Func<string, IDictionary<string, object>, TimeSpan?, bool, InvokeTargetSession, IDictionary<string, object>> runWorkflowHandler)
+        {
+            _runWorkflowHandler = runWorkflowHandler;
+        }
+
+        /// <summary>
+        /// Invokes the Main.xaml
+        /// </summary>
+        public void Main()
+        {
+            var result = _runWorkflowHandler(@"Main.xaml", new Dictionary<string, object>{}, default, default, default);
+        }
+    }
+}
